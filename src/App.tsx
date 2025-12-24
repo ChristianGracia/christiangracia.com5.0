43da1925752ffff2afe8e971b40c7c8c802a740a
christian gracia <christianmgracia@gmail.com>
2025-12-24 12:50:57 -0800

    feat: added initial light/dark mode solution

--- src/App.tsx
+++ src/App.tsx
@@ -6,19 +6,74 @@ import { About } from "./views/About";
 import { Contact } from "./views/Contact";
 import Header from "./components/Header";
 import { SITE_THEME } from "./enums/enums";
+import {
+  Box,
+  createTheme,
+  ThemeProvider,
+  useColorScheme,
+  useMediaQuery,
+} from "@mui/material";
+import { useState } from "react";
+import type { PaletteMode } from "@mui/material/styles";
 
 function App() {
+  const prefersDarkMode = useMediaQuery("(prefers-color-scheme: dark)");
+  const itemInStorage = localStorage?.getItem(SITE_THEME.DARK);
+  console.log("item in storage: " + itemInStorage);
+  const isPrefSet = localStorage?.getItem(SITE_THEME.DARK) === "true";
+  console.log("is dark mode set: " + isPrefSet);
+  console.log("user preferences: " + prefersDarkMode);
+  const [isDarkMode, setIsDarkMode] = useState(
+    isPrefSet ? true : prefersDarkMode
+  );
+
+  const { mode, setMode } = useColorScheme();
+  // if (!mode) {
+  //   return null;
+  // }
+
   const handleChangeTheme = (isDarkModeEnabled: boolean) => {
     console.log(isDarkModeEnabled);
+    setIsDarkMode(isDarkModeEnabled);
+    setMode(!isDarkModeEnabled ? "light" : "dark");
     if (isDarkModeEnabled) {
       localStorage.setItem(SITE_THEME.DARK, "true");
     } else {
       localStorage.removeItem(SITE_THEME.DARK);
     }
   };
+
+  const appTheme = createTheme({
+    palette: {
+      mode: (isDarkMode ? SITE_THEME.DARK : SITE_THEME.LIGHT) as PaletteMode,
+    },
+    colorSchemes: {
+      light: {
+        palette: {
+          primary: {
+            main: "#FF5733",
+          },
+          // ...other tokens
+        },
+      },
+      dark: {
+        palette: {
+          primary: {
+            main: "#E0C2FF",
+          },
+          // ...other tokens
+        },
+      },
+    },
+  });
+
   return (
-    <>
-      <Header handleChangeTheme={handleChangeTheme} />
+    <ThemeProvider theme={appTheme} defaultMode={"dark"}>
+      <Header
+        handleChangeTheme={handleChangeTheme}
+        isDarkModeEnabled={isDarkMode}
+      />
+      <Box sx={{ bgcolor: "primary.main" }}>text</Box>
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
@@ -27,7 +82,7 @@ function App() {
           <Route path="/contact" element={<Contact />} />
         </Routes>
       </BrowserRouter>
-    </>
+    </ThemeProvider>
   );
 }
 
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,13 +3,16 @@ import { type FC } from "react";
 
 type HeaderProps = {
   handleChangeTheme: (isDarkModeEnabled: boolean) => void;
+  isDarkModeEnabled: boolean;
 };
 
-const Header: FC<HeaderProps> = ({ handleChangeTheme }) => {
+const Header: FC<HeaderProps> = ({ handleChangeTheme, isDarkModeEnabled }) => {
+  console.log("in header, is checked? " + isDarkModeEnabled);
   return (
     <>
       <FormControlLabel
         control={<Switch />}
+        checked={isDarkModeEnabled}
         label="Light / Dark"
         onChange={(_ev, val) => handleChangeTheme(val)}
       />
